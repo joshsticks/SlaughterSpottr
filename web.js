e4dd504b3884ffba4a6a767bd36ca7f8b06258e9
Matthew Kesler <mkesler@captechconsulting.com>
2012-09-25 10:01:58 -0400

    changes

--- web.js
+++ web.js
@@ -3,7 +3,27 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  //response.send('Hello World!');
+  url = URL.parse(request.url);
+ 
+  if (request.method == 'GET') {
+    path = url.pathname.split('/');
+    resources = path[path.length - 2];
+    id = path[path.length - 1];
+ 
+    if (resources == 'messages') {
+      message = 'my message';
+ 
+      var body = {
+        "message" : message
+      };
+ 
+      response.writeHead(200, {
+        'Content-Type' : 'application/json'
+      });
+      response.end(JSON.stringify(body));
+    }
+  }
 });
 
 var port = process.env.PORT || 5000;
