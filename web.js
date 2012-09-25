e4a7ca444cc6ac1967ee3c3a590dceccb6fe240f
Matthew Kesler <mkesler@captechconsulting.com>
2012-09-25 12:22:39 -0400

    pg

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var pg = require('pg');
 
 var app = express.createServer(express.logger());
 
 app.get('/api/count', function(request, response) {
   response.send({count:12});
+});
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  var query = client.query('SELECT * FROM your_table');
+
+  query.on('row', function(row) {
+    console.log(JSON.stringify(row));
+  });
 });
 
 var port = process.env.PORT || 5000;
