3ba39c2534bf7544f7717111c3f2ae25601be167
Matthew Kesler <mkesler@captechconsulting.com>
2012-09-25 12:21:11 -0400

    remove db

--- web.js
+++ web.js
@@ -1,20 +1,6 @@
-var express = require('express'),
-    path = require("path"),
-    mongoose = require('mongoose');
+var express = require('express');
 
 var app = express.createServer(express.logger());
-
-// Database
-mongoose.connect('mongodb://localhost/ecomm_database');
-
-// Config
-app.configure(function () {
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(application_root, "public")));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
 
 app.get('/api/count', function(request, response) {
   response.send({count:12});
