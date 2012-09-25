bc50da294053fdc0ae1e5736cacfbe17269431e5
Matthew Kesler <mkesler@captechconsulting.com>
2012-09-25 12:18:55 -0400

    db

--- web.js
+++ web.js
@@ -1,6 +1,20 @@
-var express = require('express');
+var express = require('express'),
+    path = require("path"),
+    mongoose = require('mongoose');
 
 var app = express.createServer(express.logger());
+
+// Database
+mongoose.connect('mongodb://localhost/ecomm_database');
+
+// Config
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
 app.get('/api/count', function(request, response) {
   response.send({count:12});
