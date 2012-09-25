c32280e503f0d5f4982ee81d0e5bd8f36e31a194
Matthew Kesler <mkesler@captechconsulting.com>
2012-09-25 12:46:17 -0400

    yeah

--- web.js
+++ web.js
@@ -9,9 +9,15 @@ app.get('/api/count', function(request, response) {
 
 app.get('/api/list', function(request, response) {
   response.send({
-    1: { lat:37.546551, lon:-77.451382, date: new Date().toString() },
-    2: { lat:37.545866, lon:-77.456539, date: new Date().toString() }
+    1: { lat:37.546551, lon:-77.451382, date: '09/24/2012' },
+    2: { lat:37.545866, lon:-77.456539, date: '09/21/2012' },
+    3: { lat:40.052939, lon:-75.129876, date: '09/02/2012' },
+    4: { lat:37.677435, lon:-77.549744, date: '09/14/2012' }
   });
+});
+
+app.put('/api/add', function(request, response) {
+  response.send('added');
 });
 
 var port = process.env.PORT || 5000;
