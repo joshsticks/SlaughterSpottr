e1c11a3a362158ce567771c12887868e1a29b8de
Matthew Kesler <mkesler@captechconsulting.com>
2012-09-25 09:49:14 -0400

    change

--- server.js
+++ server.js
@@ -1,38 +1,36 @@
-var http = require('http');
-var URL = require('url');
- 
-database = {
-  "1" : "Hello, world!",
-  "2" : "Lorem Ipsum"
-};
- 
-handler = function(req, res) {
-  url = URL.parse(req.url);
- 
-  if (req.method == 'GET') {
-    path = url.pathname.split('/');
-    resources = path[path.length - 2];
-    id = path[path.length - 1];
- 
-    if (resources == 'messages') {
-      message = database[id];
- 
-      var body = {
-        "message" : message
-      };
- 
-      res.writeHead(200, {
-        'Content-Type' : 'application/json'
-      });
-      res.end(JSON.stringify(body));
-    }
-  }
- 
-};
- 
-host = '127.0.0.1';
-port = process.env.PORT || 5000;
+var express = require('express');
 
-http.createServer(handler).listen(port, host);
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
  
-console.log('Server running at http://' + host + ':' + port + '/');
+// handler = function(req, res) {
+//   url = URL.parse(req.url);
+ 
+//   if (req.method == 'GET') {
+//     path = url.pathname.split('/');
+//     resources = path[path.length - 2];
+//     id = path[path.length - 1];
+ 
+//     if (resources == 'messages') {
+//       message = database[id];
+ 
+//       var body = {
+//         "message" : message
+//       };
+ 
+//       res.writeHead(200, {
+//         'Content-Type' : 'application/json'
+//       });
+//       res.end(JSON.stringify(body));
+//     }
+//   }
+ 
+// };
