fe92cdde41e03d0d8d6145d4af4604842710bcd9
Matthew Kesler <mkesler@captechconsulting.com>
2012-09-25 12:32:29 -0400

    blah

--- web.js
+++ web.js
@@ -1,5 +1,15 @@
 var express = require('express');
-var pg = require('pg');
+//var pg = require('pg');
+
+var pg = require('pg').native
+  , connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/dailyjs'
+  , client
+  , query;
+
+client = new pg.Client(connectionString);
+client.connect();
+query = client.query('SELECT * FROM mytable');
+query.on('end', function() { client.end(); });
 
 var app = express.createServer(express.logger());
 
@@ -7,13 +17,13 @@ app.get('/api/count', function(request, response) {
   response.send({count:12});
 });
 
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  var query = client.query('SELECT * FROM your_table');
+// pg.connect(process.env.DATABASE_URL, function(err, client) {
+//   var query = client.query('SELECT * FROM your_table');
 
-  query.on('row', function(row) {
-    console.log(JSON.stringify(row));
-  });
-});
+//   query.on('row', function(row) {
+//     console.log(JSON.stringify(row));
+//   });
+// });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
