ce8d51803b11cebacb2c1aa55e16e72a0ef0a46a
Matthew Kesler <mkesler@captechconsulting.com>
2012-09-26 14:23:53 -0400

    m

--- web.js
+++ web.js
@@ -3,15 +3,11 @@ var pg = require('pg').native;
 
 var client = new pg.Client('postgres://jzjnyhhuadkjih:nFfX92G0Vo5oQTnoXThkaT9MKV@ec2-54-243-228-4.compute-1.amazonaws.com:5432/d2le9eigheli01');
 client.connect();
-var query = client.query('CREATE TABLE locations (lat VARCHAR(64), lon VARCHAR(64), date DATE)');
-query.on('end', function() { client.end(); });
+var query2 = client.query('SELECT * FROM locations');
 
-//var query1 = client.query("INSERT INTO locations (lat, lon) VALUES ('123', '456')");
-//var query2 = client.query('SELECT * FROM locations');
-
-//query2.on('row', function(result) {
-//  console.log(result);
-//});
+query2.on('row', function(result) {
+ console.log(result);
+});
 
 
 //query.on('end', function() { client.end(); });
