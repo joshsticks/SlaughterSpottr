2f7f9afbb2c0b6b59cd727bbed81c157d3c8732d
Matthew Kesler <mkesler@captechconsulting.com>
2012-09-26 13:31:37 -0400

    m

--- web.js
+++ web.js
@@ -1,13 +1,7 @@
 var express = require('express');
 var pg = require('pg');
 
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  var query = client.query('SELECT * FROM your_table');
-
-  query.on('row', function(row) {
-    console.log(JSON.stringify(row));
-  });
-});
+console.log('db url: ' + process.env.DATABASE_URL);
 
 var app = express.createServer(express.logger());
 
