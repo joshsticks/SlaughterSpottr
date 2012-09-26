3f8f418293ea54170f87fe20ea2eba6b69a9f59f
Matthew Kesler <mkesler@captechconsulting.com>
2012-09-26 13:47:19 -0400

    m

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var pg = require('pg');
 
 var client = new pg.Client('postgres://jzjnyhhuadkjih:nFfX92G0Vo5oQTnoXThkaT9MKV@ec2-54-243-228-4.compute-1.amazonaws.com:5432/d2le9eigheli01');
 client.connect();
-//var query = client.query('CREATE TABLE locations (string string date)');
+var query = client.query('CREATE TABLE locations (string string date)');
 //query.on('end', function() { client.end(); });
 
 var app = express.createServer(express.logger());
