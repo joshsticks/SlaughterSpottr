e7ba62beae2723b54cbe5e7080e6717b682cf679
Josh Stickles <sticklesjb@gmail.com>
2013-08-26 09:38:39 -0400

    added in my own pg db

--- schema.js
+++ schema.js
@@ -1,6 +1,6 @@
 var pg = require('pg').native;
 
-var client = new pg.Client('postgres://jzjnyhhuadkjih:nFfX92G0Vo5oQTnoXThkaT9MKV@ec2-54-243-228-4.compute-1.amazonaws.com:5432/d2le9eigheli01');
+var client = new pg.Client('postgres://hzrkeqokpomkdb:Nbr2u-1yKKU01Go5bS9BxhM2aG@ec2-54-221-240-24.compute-1.amazonaws.com:5432/d5nd6olcvp8h0i');
 client.connect();
 var query = client.query('CREATE TABLE locations (lat VARCHAR(64), lon VARCHAR(64), date DATE)');
 query.on('end', function() { client.end(); });
